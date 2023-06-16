d228a37435f540ee1162e097df1175b1a0152f6b
amelamelame <amelshab2018@gmail.com>
2023-06-16 17:03:02 +0530

    fourth commit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Form from './Form';
 
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
